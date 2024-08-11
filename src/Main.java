45a0f53b0b6eb46a6e673ea80633d7e1e7f43d48
davi <daviafonsdias@gmail.com>
2024-08-11 07:42:22 -0300

    Eu to cansado chefe

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("oi");
+        Object[] options = {"Filme", "Concerto", "Teatro"};
+        JOptionPane.showOptionDialog(null,
+        "Qual tipo de evento você vai Comprar o ingreso ",
+                "Bilheteria Cinemark",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.INFORMATION_MESSAGE,
+                null,options,options[0]);
     }
 }
